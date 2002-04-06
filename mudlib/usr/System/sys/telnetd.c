69adedac977e25c39a5d75dbbf8717567bb14364
angelbob <angelbob@71c14604-2bbe-4e77-8db8-9244a9b71533>
2002-04-06 20:40:46 +0000

    Added TelnetD ability to suspend or shut down, either way giving an alternate user message.

--- mudlib/usr/System/sys/telnetd.c
+++ mudlib/usr/System/sys/telnetd.c
@@ -2,17 +2,15 @@
 #include <config.h>
 #include <log.h>
 
-#define WELCOME_MESSAGE "/data/text/welcome.msg"
-
-static string welcome;
+static string welcome_banner, suspended_banner, shutdown_banner;
 static object logd;
-static int    suspended;
+static int    suspended, shutdown;
 
 void upgraded(varargs int clone);
 
 static void create(varargs int clone) {
   if(clone) {
-    error("Can't clone Telnetd!");
+    error("Can't clone TelnetD!");
   }
 
   upgraded();
@@ -22,21 +20,30 @@ void upgraded(varargs int clone) {
   if(!find_object(SYSTEM_USER)) { compile_object(SYSTEM_USER); }
 
   logd = find_object(LOGD);
-  welcome = ::read_file(WELCOME_MESSAGE);
-  if(!welcome)
-    error("Can't read welcome message file " + WELCOME_MESSAGE + "!");
+  welcome_banner = ::read_file(WELCOME_MESSAGE);
+  if(!welcome_banner)
+    error("Can't read 'welcome' message file " + WELCOME_MESSAGE + "!");
+  suspended_banner = ::read_file(SUSPENDED_MESSAGE);
+  if(!suspended_banner)
+    error("Can't read 'suspended' message file " + SUSPENDED_MESSAGE + "!");
+  shutdown_banner = ::read_file(SHUTDOWN_MESSAGE);
+  if(!shutdown_banner)
+    error("Can't read 'shutdown' message file " + SHUTDOWN_MESSAGE + "!");
 }
 
-void suspend_input(void) {
+void suspend_input(int shutdownp) {
   if(suspended)
     LOGD->write_syslog("Suspended again without release!", LOG_ERR);
 
   suspended = 1;
+  if(shutdownp)
+    shutdown = 1;
 }
 
 void release_input(void) {
   if(!suspended)
     LOGD->write_syslog("Released without suspend!", LOG_ERR);
+
   suspended = 0;
 }
 
@@ -47,10 +54,19 @@ object select(string str)
 
 int query_timeout(object connection)
 {
+  if(suspended || shutdown)
+    return -1;
+
   return DEFAULT_TIMEOUT;
 }
 
 string query_banner(object connection)
 {
-  return welcome;
+  if(shutdown)
+    return shutdown_banner;
+
+  if(suspended)
+    return suspended_banner;
+
+  return welcome_banner;
 }
