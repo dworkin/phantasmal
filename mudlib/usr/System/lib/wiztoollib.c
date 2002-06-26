3ca1ffd62ce02e4371ce0f994044b25c60bde65f
angelbob <angelbob@71c14604-2bbe-4e77-8db8-9244a9b71533>
2002-06-26 20:53:59 +0000

    Added compile for SIMPLE_ROOM and SIMPLE_DETAIL.

--- mudlib/usr/System/lib/wiztoollib.c
+++ mudlib/usr/System/lib/wiztoollib.c
@@ -59,6 +59,10 @@ static void create(varargs int clone)
       auto::compile_object(UNQ_DTD);
     if(!find_object(SIMPLE_PORTABLE))
       auto::compile_object(SIMPLE_PORTABLE);
+    if(!find_object(SIMPLE_ROOM))
+      auto::compile_object(SIMPLE_ROOM);
+    if(!find_object(SIMPLE_DETAIL))
+      auto::compile_object(SIMPLE_DETAIL);
   }
 }
 
