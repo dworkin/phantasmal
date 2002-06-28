515bc46f6b7f71f7fde9e43423fd2010dbf77d3c
angelbob <angelbob@71c14604-2bbe-4e77-8db8-9244a9b71533>
2002-06-28 22:20:04 +0000

    Made zoned a DTD_UNQABLE type.

--- mudlib/usr/common/sys/zoned.c
+++ mudlib/usr/common/sys/zoned.c
@@ -5,6 +5,8 @@
 
 /* ZoneD -- daemon that tracks zones -- currently mostly hardcoded */
 
+inherit dtd DTD_UNQABLE;
+
 /* Prototypes */
 void upgraded(varargs int clone);
 
@@ -14,12 +16,16 @@ static void create(varargs int clone) {
   if(clone)
     error("Cloning zoned is not allowed!");
 
+  dtd::create(clone);
+
   if(!find_object(UNQ_DTD)) compile_object(UNQ_DTD);
 
   upgraded();
 }
 
 void upgraded(varargs int clone) {
+  set_dtd_file(ZONED_DTD);
+  dtd::upgraded(clone);
   zone_table = ({ ({ "Unzoned", ([ ]) }),
 		    ({ "Miskatonic University", ([ ]) }),
 		    ({ "Innsmouth", ([ ]) }),
@@ -27,8 +33,63 @@ void upgraded(varargs int clone) {
 }
 
 void destructed(int clone) {
-
+  dtd::destructed(clone);
 }
+
+/******* Functions for DTD_UNQABLE *************************/
+
+mixed* to_dtd_unq(void) {
+  int    ctr, highseg, zone;
+  mixed *tmp;
+
+  highseg = OBJNUMD->get_highest_segment();
+  tmp = ({ "zones", ({ }) });
+  for(ctr = 0; ctr < highseg; ctr++) {
+    if(!OBJNUMD->get_segment_owner(ctr))
+      continue;
+
+    zone = OBJNUMD->get_segment_zone(ctr);
+    tmp[1] += ({ ({ "segment",
+		      ({ ({ "segnum", ctr }),
+			   ({ "zonenum", zone })
+			   })
+		      })
+		   });
+  }
+
+  return tmp;
+}
+
+void from_dtd_unq(mixed* unq) {
+  mixed *zones, *segment;
+
+  if(sizeof(unq) > 2)
+    error("There should be only one zones section in the ZONED file!");
+
+  if(unq[0] != "zones")
+    error("Unrecognized section in ZONED file -- must start with 'zones'!");
+
+  zones = unq[1];
+  while(sizeof(zones)) {
+    /* Everything in the zones entry must be a segment. */
+    segment = zones[1];
+
+    /* Remove that segment, move on */
+    zones = zones[2..];
+
+  }
+}
+
+void write_to_file(string filename) {
+  if(previous_program() != "/usr/System/initd")
+    error("Only INITD may instruct the ZONED to write files!");
+
+  LOGD->write_syslog("Writing to file " + filename);
+  dtd::write_to_file(filename);
+}
+
+
+/******* Regular ZONED functions ***************************/
 
 int num_zones(void) {
   return sizeof(zone_table);
