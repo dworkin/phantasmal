43ce408bfe6329ab67c95a3e7e202ecef9bbefab
angelbob <angelbob@71c14604-2bbe-4e77-8db8-9244a9b71533>
2003-01-29 09:14:15 +0000

    New debug messages, generally works better.  Can be created successfully with @new_mob.

--- mudlib/usr/common/obj/wander_mobile.c
+++ mudlib/usr/common/obj/wander_mobile.c
@@ -14,12 +14,13 @@ void upgraded(varargs int clone);
 static void create(varargs int clone) {
   ::create(clone);
 
-  upgraded();
+  upgraded(clone);
 }
 
 
 void upgraded(varargs int clone) {
-  if(!registered) {
+  if(clone && !registered) {
+    LOGD->write_syslog("Setting up periodic call_out in wander mobile");
     TIMED->periodic_call_out(TIMED_MUD_MINUTE, "__move_hook");
     registered = 1;
   }
@@ -34,5 +35,13 @@ void __move_hook(void) {
     error("wander_mobile::__move_hook should only be called by TIMED!");
   }
 
-  LOGD->write_syslog("Yup, it worked.");
+  LOGD->write_syslog("A wander_mobile should move randomly now.");
+}
+
+void from_dtd_unq(mixed* unq) {
+  /* Set the body, location and number fields */
+  unq = mobile_from_dtd_unq(unq);
+
+  /* Wandering mobiles don't actually (yet) have any additional data.
+     So we can just return. */
 }
