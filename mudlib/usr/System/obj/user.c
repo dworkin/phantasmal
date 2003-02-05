c2fe6542a1a4473d825b34af62ca96ff139c9fda
angelbob <angelbob@71c14604-2bbe-4e77-8db8-9244a9b71533>
2003-02-05 21:12:19 +0000

    User object now uses new command-set library.

--- mudlib/usr/System/obj/user.c
+++ mudlib/usr/System/obj/user.c
@@ -1,4 +1,4 @@
-/* $Header: /cvsroot/phantasmal/mudlib/usr/System/obj/user.c,v 1.28 2003/02/03 22:20:47 angelbob Exp $ */
+/* $Header: /cvsroot/phantasmal/mudlib/usr/System/obj/user.c,v 1.29 2003/02/05 21:12:19 angelbob Exp $ */
 
 #include <kernel/kernel.h>
 #include <kernel/user.h>
@@ -13,6 +13,7 @@
 inherit LIB_USER;
 inherit user API_USER;
 inherit rsrc API_RSRC;
+inherit cmd  "/usr/System/lib/commandsetlib";
 
 #define STATE_NORMAL            0
 #define STATE_LOGIN             1
@@ -25,17 +26,14 @@ string name;	                /* user name */
 string password;		/* user password */
 int    locale;                  /* chosen output locale */
 int    channel_subs;            /* user channel subscriptions */
-int    body_num;                /* portable number of body */
-int    num_lines;               /* number of terminal lines */
+int    body_num;                /* number of body object */
+int    num_lines;               /* how many lines on terminal */
 
 /* User-state processing stack */
 private object* state_stack;
 private mixed   state_data;
 
 private object  scroll_state;
-
-/* Commandset processing */
-private mixed* command_sets;
 
 /* Random unsaved */
 static string Name;		/* capitalized user name */
@@ -52,12 +50,11 @@ static object mobile;           /* Mobile for body object */
 static object location;         /* Location of body */
 
 /* Prototypes */
-void upgraded(void);
+        void   upgraded(void);
 static  int    process_message(string str);
 static  void   print_prompt(void);
 private int    name_is_forbidden(string name);
-private mixed* load_command_sets_file(string filename);
-void push_state(object state);
+        void   push_state(object state);
 
 /* Macros */
 #define NEW_PHRASE(x) PHRASED->new_simple_english_phrase(x)
@@ -72,10 +69,10 @@ static void create(int clone)
   if (clone) {
     user::create();
     rsrc::create();
+    cmd::create();
     state = ([ ]);
 
     state_stack = ({ });
-    command_sets = nil;
 
     /* Default to enUS locale */
     locale = PHRASED->language_by_name("english");
@@ -93,123 +90,7 @@ void upgraded(void) {
   if(!find_object(MOBILE_PORTABLE)) { compile_object(MOBILE_PORTABLE); }
   if(!find_object(US_SCROLL_TEXT)) { compile_object(US_SCROLL_TEXT); }
 
-  command_sets = load_command_sets_file(USER_COMMANDS_FILE);
-  if(!command_sets) {
-    LOGD->write_syslog("Command_sets is Nil!", LOG_FATAL);
-  }
-}
-
-
-int num_command_sets(int loc) {
-  if(!command_sets[loc])
-    return 0;
-
-  return sizeof(command_sets[loc]);
-}
-
-mixed* query_command_sets(int loc, int num, string cmd) {
-  mixed* ret;
-
-  ret = command_sets[loc][num][cmd];
-
-  if(ret) {
-    return ret[..];
-  } else {
-    return nil;
-  }
-}
-
-private void add_commands_to_set(mixed* tmp_cmd, int loc, string cmds) {
-  mixed* entries;
-  int    ctr;
-  string cmd, func;
-
-  if(!tmp_cmd[loc]) {
-    tmp_cmd[loc] = ({ ([ ]) });
-  }
-  if(!sizeof(tmp_cmd[loc])) {
-    tmp_cmd[loc] += ({ ([ ]) });
-  }
-  if(!tmp_cmd[loc][0]) {
-    tmp_cmd[loc][0] = ([ ]);
-  }
-
-  entries = explode(cmds, ",");
-  for(ctr = 0; ctr < sizeof(entries); ctr++) {
-    entries[ctr] = STRINGD->trim_whitespace(entries[ctr]);
-    if(sscanf(entries[ctr], "%s/%s", cmd, func) != 2) {
-      error("Command entry doesn't match format: '" + entries[ctr]
-	    + "'");
-    }
-    cmd = STRINGD->trim_whitespace(cmd);
-    func = STRINGD->trim_whitespace(func);
-    tmp_cmd[loc][0][cmd] = ({ func });
-  }
-}
-
-private mixed* load_command_sets_file(string filename) {
-  mixed*  tmp_cmd, *unq;
-  string  file;
-  int     ctr, err, loc;
-
-  file = read_file(filename);
-  if(!file) {
-    LOGD->write_syslog("Couldn't find file " + filename
-		       + ", not updating command_sets!", LOG_ERR);
-    return command_sets;
-  }
-
-  if(!find_object(PHRASED)) { compile_object(PHRASED); }
-  LOGD->write_syslog("Allocating command_sets", LOG_NORMAL);
-  tmp_cmd = allocate(PHRASED->num_locales());
-
-  unq = UNQ_PARSER->basic_unq_parse(file);
-  if(!unq) {
-    LOGD->write_syslog("Couldn't parse file " + filename
-		       + " as UNQ!, not updating command_sets!", LOG_ERR);
-    return command_sets;
-  }
-
-  err = 0;
-  for(ctr = 0; ctr < sizeof(unq); ctr += 2) {
-    /* Filter out whitespace */
-    if(!unq[ctr] || STRINGD->is_whitespace(unq[ctr])) {
-      if(typeof(unq[ctr + 1]) != T_STRING
-	 || !STRINGD->is_whitespace(unq[ctr + 1])) {
-	LOGD->write_syslog("Error: not type T_STRING", LOG_ERR);
-	err = 1;
-	break;
-      }
-      continue;
-    }
-    if(typeof(unq[ctr + 1]) != T_STRING) {
-      LOGD->write_syslog("Error: not type T_STRING: '"
-			 + STRINGD->mixed_sprint(unq[ctr + 1]) + "'/"
-			 + typeof(unq[ctr + 1]), LOG_ERR);
-      err = 1;
-      break;
-    }
-
-    /* Trim whitespace in remaining entries */
-    unq[ctr] = STRINGD->trim_whitespace(unq[ctr]);
-    unq[ctr+1] = STRINGD->trim_whitespace(unq[ctr+1]);
-
-    loc = PHRASED->language_by_name(unq[ctr]);
-    if(loc == -1) {
-      LOGD->write_syslog("Error: unknown locale '" + unq[ctr] + "'", LOG_ERR);
-      err = 1;
-      break;
-    }
-
-    add_commands_to_set(tmp_cmd, loc, unq[ctr + 1]);
-  }
-  if(err) {
-    LOGD->write_syslog("Error decoding UNQ, not updating command_sets!",
-		       LOG_ERR);
-    return command_sets;
-  }
-
-  return tmp_cmd;
+  cmd::upgraded();
 }
 
 
