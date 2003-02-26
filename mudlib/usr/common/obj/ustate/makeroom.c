67c936ff66843132b0f44d2ecad0a373828ad403
angelbob <angelbob@71c14604-2bbe-4e77-8db8-9244a9b71533>
2003-02-26 09:56:40 +0000

    The @make_room command has successfully made its first room.  All common room features seem to work fine, though there are a couple of rough edges.

--- mudlib/usr/common/obj/ustate/makeroom.c
+++ mudlib/usr/common/obj/ustate/makeroom.c
@@ -14,6 +14,8 @@ private string brief_desc;
 private string glance_desc;
 private string look_desc;
 private string examine_desc;
+private string nouns;
+private string adjectives;
 
 
 /* Valid substate values */
@@ -22,6 +24,8 @@ private string examine_desc;
 #define SS_PROMPT_GLANCE_DESC       3
 #define SS_PROMPT_LOOK_DESC         4
 #define SS_PROMPT_EXAMINE_DESC      5
+#define SS_PROMPT_NOUNS             6
+#define SS_PROMPT_ADJECTIVES        7
 
 /* Input function return values */
 #define RET_NORMAL            1
@@ -34,11 +38,16 @@ static int  prompt_brief_desc_input(string input);
 static int  prompt_glance_desc_input(string input);
 static void prompt_look_desc_data(mixed data);
 static void prompt_examine_desc_data(mixed data);
+static int  prompt_nouns_input(string input);
+static int  prompt_adjectives_input(string input);
+
+#define NEW_PHRASE(x) PHRASED->new_simple_english_phrase(x)
 
 
 static void create(varargs int clone) {
   ::create();
   if(!find_object(US_ENTER_DATA)) compile_object(US_ENTER_DATA);
+  if(!find_object(LWO_PHRASE)) compile_object(LWO_PHRASE);
   if(clone) {
     substate = SS_PROMPT_ROOM_NUMBER;
   }
@@ -74,6 +83,12 @@ int from_user(string input) {
     send_string("Internal error in state machine!  Cancelling OLC!\r\n");
     pop_state();
     return MODE_ECHO;
+    break;
+  case SS_PROMPT_NOUNS:
+    ret = prompt_nouns_input(input);
+    break;
+  case SS_PROMPT_ADJECTIVES:
+    ret = prompt_adjectives_input(input);
     break;
   default:
     send_string("Unrecognized state!  Cancelling OLC!\r\n");
@@ -153,6 +168,8 @@ void pass_data(mixed data) {
 }
 
 static int prompt_room_number_input(string input) {
+  string segown;
+
   if(!input || STRINGD->is_whitespace(input)) {
     /* Autoassign */
     room_number = -1;
@@ -171,6 +188,19 @@ static int prompt_room_number_input(string input) {
       send_string("That doesn't appear to be a legal room number.\r\n");
       send_string("Enter a (positive, nonzero) room number or hit enter.\r\n");
 
+      return RET_NORMAL;
+    }
+    if(MAPD->get_room_by_num(room_number)) {
+      send_string("There is already a room with that number.\r\n");
+      send_string("Enter a room number, type 'quit' or hit enter.\r\n");
+
+      return RET_NORMAL;
+    }
+    segown = OBJNUMD->get_segment_owner(room_number / 100);
+    if(room_number >= 0 && segown && segown != MAPD) {
+      user->message("Room number " + room_number
+		    + " is in a segment reserved for non-rooms!\r\n");
+      send_string("Enter a room number, type 'quit' or hit enter.\r\n");
       return RET_NORMAL;
     }
 
@@ -273,7 +303,8 @@ static void prompt_look_desc_data(mixed data) {
   send_string("Now, enter a multiline 'examine' description.  This is what an"
 	      + " observer would\r\n");
   send_string("note about this object with careful scrutiny.\r\n");
-  send_string("Or just hit enter and it will default to the look desc.\r\n");
+  send_string("Or hit '~' and enter and it will default to the look "
+	      + "description.\r\n");
 
   edit_state = clone_object(US_ENTER_DATA);
   if(edit_state) {
@@ -301,6 +332,113 @@ static void prompt_examine_desc_data(mixed data) {
     examine_desc = STRINGD->trim_whitespace(data);
   }
 
-  send_string("Test complete.\r\n");
-  pop_state();
+  substate = SS_PROMPT_NOUNS;
+
+  send_string("\r\nOkay, now let's get a list of the nouns and adjectives "
+	      + "you can\r\n");
+  send_string("use to refer to the object.  For reference, we'll also let you"
+	      + " see");
+  send_string("the short descriptions you supplied.\r\n");
+  send_string("Brief:  " + brief_desc + "\r\n");
+  send_string("Glance: " + glance_desc + "\r\n");
+  send_string("\r\nNow, give a space-separated list of nouns to refer to this"
+	      + " object.\r\n");
+  send_string("Example: sword blade hilt weapon pommel\r\n\r\n");
+
+  /* Don't return anything, this is a void function */
+}
+
+static int prompt_nouns_input(string input) {
+
+  if(!input || STRINGD->is_whitespace(input)) {
+    send_string("Nope.  You'll want at least one noun.  Try again.\r\n");
+    send_string("Brief:  " + brief_desc + "\r\n");
+    send_string("Glance: " + glance_desc + "\r\n");
+    send_string("\r\nNow, give a space-separated list of nouns to refer to "
+		+ "this object.\r\n");
+    send_string("Example: sword blade weapon\r\n\r\n");
+    return RET_NORMAL;
+  }
+
+  substate = SS_PROMPT_ADJECTIVES;
+  nouns = STRINGD->trim_whitespace(input);
+  send_string("\r\nGood.  Next, do the same for adjectives.\r\n");
+  send_string("Example: heavy gray dull\r\n");
+
+  return RET_NORMAL;
+}
+
+private mixed process_words(string input) {
+  object  phr;
+  string* words;
+  int     ctr;
+
+  words = explode(input, " ");
+
+  for(ctr = 0; ctr < sizeof(words); ctr++) {
+    words[ctr] = STRINGD->to_lower(words[ctr]);
+  }
+
+  phr = new_object(LWO_PHRASE);
+  phr->set_content_by_lang(get_user()->get_locale(),
+			   implode(words[0..],","));
+
+  return phr;
+}
+
+static int prompt_adjectives_input(string input) {
+  string segown;
+  object room, phr;
+  int    zonenum;
+
+  send_string("\r\nGood.  Creating object...\r\n");
+  adjectives = STRINGD->trim_whitespace(input);
+
+  segown = OBJNUMD->get_segment_owner(room_number / 100);
+
+  if((MAPD->get_room_by_num(room_number))
+     || (room_number >= 0 && segown && segown != MAPD)) {
+    /* TODO:  allocate room when num is entered? */
+
+    send_string("Somebody else has created an object with that number!\r\n");
+    send_string("Not sure how to deal with this conflict.  Quitting.\r\n");
+
+    return RET_POP_STATE;
+  }
+
+  room = clone_object(SIMPLE_ROOM);
+  zonenum = -1;
+  if(room_number < 0) {
+    zonenum = ZONED->get_zone_for_room(get_user()->get_location());
+    if(zonenum < 0) {
+      LOGD->write_syslog("Odd, zone is less than 0 in @make_room...",
+			 LOG_WARN);
+      zonenum = 0;
+    }
+  }
+  MAPD->add_room_to_zone(room, room_number, zonenum);
+
+  zonenum = ZONED->get_zone_for_room(room);
+
+  send_string("Added room #" + room->get_number()
+	      + " to zone #" + zonenum
+	      + " (" + ZONED->get_name_for_zone(zonenum) + ")" + ".\r\n");
+
+  /* Set various descriptions */
+  phr = room->get_brief();
+  phr->set_content_by_lang(get_user()->get_locale(), brief_desc);
+  phr = room->get_glance();
+  phr->set_content_by_lang(get_user()->get_locale(), glance_desc);
+  phr = room->get_look();
+  phr->set_content_by_lang(get_user()->get_locale(), look_desc);
+
+  if(examine_desc && !STRINGD->is_whitespace(examine_desc)) {
+    room->set_examine(room, NEW_PHRASE(examine_desc));
+  }
+
+  /* Set nouns and adjectives */
+  room->add_noun(process_words(nouns));
+  room->add_adjective(process_words(adjectives));
+
+  return RET_POP_STATE;
 }
