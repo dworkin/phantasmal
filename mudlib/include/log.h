6ed0f3afd3afe398c53b9bc9f20f8da35a80a83f
angelbob <angelbob@71c14604-2bbe-4e77-8db8-9244a9b71533>
2003-03-02 09:54:02 +0000

    New log level, LOG_ULTRA_VERBOSE.

--- mudlib/include/log.h
+++ mudlib/include/log.h
@@ -1,15 +1,17 @@
-#define LOG_ERROR_FATAL    6
-#define LOG_ERR_FATAL      6
-#define LOG_FATAL          6
+#define LOG_ERROR_FATAL    7
+#define LOG_ERR_FATAL      7
+#define LOG_FATAL          7
 
-#define LOG_ERROR          5
-#define LOG_ERR            5
+#define LOG_ERROR          6
+#define LOG_ERR            6
 
-#define LOG_WARNING        4
-#define LOG_WARN           4
+#define LOG_WARNING        5
+#define LOG_WARN           5
 
-#define LOG_DEBUG          3
+#define LOG_DEBUG          4
 
-#define LOG_NORMAL         2
+#define LOG_NORMAL         3
 
-#define LOG_VERBOSE        1
+#define LOG_VERBOSE        2
+
+#define LOG_ULTRA_VERBOSE  1
