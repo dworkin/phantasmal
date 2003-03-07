4919b155a79402113f7d40f6ec318d3925a53b49
angelbob <angelbob@71c14604-2bbe-4e77-8db8-9244a9b71533>
2003-03-07 00:09:06 +0000

    Removed objflags now that ROOM has it.

--- mudlib/usr/common/obj/simple_portable.c
+++ mudlib/usr/common/obj/simple_portable.c
@@ -9,14 +9,6 @@ inherit unq UNQABLE;
 
 #define PHR(x) PHRASED->new_simple_english_phrase(x)
 
-/* The objflags field contains a set of boolean object flags */
-#define OF_CONTAINER          1
-#define OF_OPEN               2
-#define OF_NODESC             4
-#define OF_OPENABLE           8
-
-int objflags;
-
 static void create(varargs int clone) {
   obj::create(clone);
   unq::create(clone);
@@ -44,7 +36,6 @@ string to_unq_text(void) {
   string ret;
   ret = "~portable{\n";
   ret += to_unq_flags();
-  ret += "~flags{" + objflags + "}\n";
   ret += "}\n";
   return ret;
 }
@@ -57,80 +48,20 @@ void from_dtd_unq(mixed* unq) {
 
   /* load text */
   for (ctr = 0; ctr < sizeof(unq[1]); ctr++) {
-    if (unq[1][ctr][0] == "flags") {
-      objflags = unq[1][ctr][1];
-    } else {
-      from_dtd_tag(unq[1][ctr][0], unq[1][ctr][1]);
-    }
+    from_dtd_tag(unq[1][ctr][0], unq[1][ctr][1]);
   }
 }
 
-/* 
- * flag overrides
- */
-
-int is_no_desc() {
-  return objflags & OF_NODESC;
-}
-
-int is_container() {
-  return objflags & OF_CONTAINER;
-}
-
-int is_open() {
-  return objflags & OF_OPEN;
-}
-
-int is_openable() {
-  return objflags & OF_OPENABLE;
-}
-
-private void set_flags(int flags, int value) {
-  if(value) {
-    objflags |= flags;
-  } else {
-    objflags &= ~flags;
-  }
-}
-
-void set_nodesc(int value) {
-  if(!SYSTEM() && previous_program() != US_MAKE_ROOM)
-    error("Only SYSTEM code can currently set an object nodesc!");
-
-  set_flags(OF_NODESC, value);
-}
-
-void set_container(int value) {
-  if(!SYSTEM() && previous_program() != US_MAKE_ROOM)
-    error("Only SYSTEM code can currently set an object as a container!");
-
-  set_flags(OF_CONTAINER, value);
-}
-
-void set_open(int value) {
-  if(!SYSTEM() && previous_program() != MOBILE
-      && previous_program() != US_MAKE_ROOM)
-    error("Only SYSTEM code can currently set an object as open!");
-
-  set_flags(OF_OPEN, value);
-}
-
-void set_openable(int value) {
-  if(!SYSTEM() && previous_program() != US_MAKE_ROOM)
-    error("Only SYSTEM code can currently set an object as openable!");
-
-  set_flags(OF_OPENABLE, value);
-}
 
 /* function which returns an appropriate error message if this object
  * isn't a container or open this one
  */
 
 private string is_open_cont() {
-  if (!(objflags & OF_CONTAINER)) {
+  if (!is_container()) {
     return "That object isn't a container!";
   }
-  if (!(objflags & OF_OPEN)) {
+  if (!is_open()) {
     return "That object isn't open!";
   }
   return nil;
@@ -150,7 +81,7 @@ string can_remove(object mover, object movee, object new_env) {
 
 string can_enter(object enter_object, int dir) {
   if (dir == DIR_TELEPORT) {
-    if (!(objflags & OF_CONTAINER)) {
+    if (!is_container()) {
       return "You can't enter that!";
     } else {
       return nil;
@@ -162,7 +93,7 @@ string can_enter(object enter_object, int dir) {
 
 string can_leave(object leave_object, int dir) {
   if (dir == DIR_TELEPORT) {
-    if (!(objflags & OF_CONTAINER)) {
+    if (!is_container()) {
       return "You can't enter that!";
     } else {
       return nil;
