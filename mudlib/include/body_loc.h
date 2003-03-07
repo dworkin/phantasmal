48f44a749b9d09b30016357dc15eab92d4edc2c5
angelbob <angelbob@71c14604-2bbe-4e77-8db8-9244a9b71533>
2003-03-07 09:43:18 +0000

    First version specifying underlying object-binding locations.  A function in /usr/System/obj/user.c will be making these useful shortly, especially once we have real object details.

---
+++ mudlib/include/body_loc.h
@@ -0,0 +1,31 @@
+/* body_loc.h
+
+   These constants are used by the SYSTEM_USER::find_objects() and
+   SYSTEM_USER::find_first_objects() calls to
+   determine what objects are visible to the viewer.  By supplying
+   a list of locations, the caller determines where, and in what
+   order, the returned objects can be found.
+*/
+
+/* This looks for objects contained in the current room.  It also
+   looks inside anything it reasonably can, such as open
+   containers or the room's details. */
+#define LOC_CURRENT_ROOM                           1
+
+/* This looks only for objects contained in the current room -- no details,
+   no inventory, nothing in containers, nothing else. */
+#define LOC_IMMEDIATE_CURRENT_ROOM                 2
+
+/* This looks for details of the current room and their details. */
+#define LOC_DETAIL_CURRENT_ROOM                    3
+
+/* This looks for objects contained in the viewer, objects in details
+   of the viewer and objects in open containers within the viewer. */
+#define LOC_INVENTORY                              4
+
+/* This looks for objects immediately contained by the viewer -- nothing
+   in containers, nothing in body details. */
+#define LOC_IMMEDIATE_INVENTORY                    5
+
+/* This looks for objects which are details of the viewer */
+#define LOC_BODY                                   6
