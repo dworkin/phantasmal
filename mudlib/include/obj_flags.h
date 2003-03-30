002890884a84cda0c84562e59d43850ba3f4cb92
angelbob <angelbob@71c14604-2bbe-4e77-8db8-9244a9b71533>
2003-03-30 20:52:48 +0000

    Moved object flags to a header.  Since we're using them in multiple places, we don't want duplicate code and different object flag values.

---
+++ mudlib/include/obj_flags.h
@@ -0,0 +1,8 @@
+/* Flags */
+/* The objflags fields in OBJECT and EXIT contain a set of boolean
+   object flags */
+#define OF_CONTAINER          1
+#define OF_OPEN               2
+#define OF_OPENABLE           8
+#define OF_LOCKED             16
+#define OF_LOCKABLE           32
