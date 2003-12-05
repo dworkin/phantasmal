259e93ee60765eb9bfc5f3f56bef5b2b78d215e7
angelbob <angelbob@71c14604-2bbe-4e77-8db8-9244a9b71533>
2003-12-05 22:24:52 +0000

    New documentation printed by install_mud.pl.  Moved start_mud to start_mud.pl to match.

--- testgame/bundled/install_mud.pl
+++ testgame/bundled/install_mud.pl
@@ -23,4 +23,14 @@ while(@lines) {
 close(CONFIGFILE);
 close(OUTFILE);
 
+print "New .dgd file created.  Copying it into place.\n";
 system("mv new_testgame.dgd testgame.dgd");
+
+print "Installation done.  If you get an error saying to run the install\n";
+print "script, you should type \"mv new_testgame.dgd testgame.dgd\" to fix\n";
+print "it.  Otherwise, you're good to go.\n\n";
+
+print "To run the MUD, try typing \"./start_mud.pl\".  If that doesn't work\n";
+print "then type \"perl start_mud.pl\".  If you don't have Perl installed,\n";
+print "you'll need to either install it or learn to run the MUD by hand.\n";
+print "It's pretty easy either way.  Enjoy!\n";
