eec06d4d4dda7b66b17f18cd17f0e084aef30b04
angelbob <angelbob@71c14604-2bbe-4e77-8db8-9244a9b71533>
2003-12-18 22:18:36 +0000

    Updated TestGame to handle new Phantasmal Tag stuff.  Not fully debugged.

--- testgame/usr/game/initd.c
+++ testgame/usr/game/initd.c
@@ -1,7 +1,10 @@
 #include <phantasmal/log.h>
 
+#include <type.h>
 #include <gameconfig.h>
 #include <config.h>
+
+static mixed* load_file_with_dtd(string file_path, string dtd_path);
 
 static void config_unq_file(void);
 static void configure_from_unq(mixed* unq);
@@ -9,6 +12,7 @@ static void configure_from_unq(mixed* unq);
 static void set_up_scripting(void);
 static void set_up_heart_beat(void);
 static void load_sould(void);
+static void load_tagd(void);
 
 static void create(void) {
   LOGD->write_syslog("**** Starting Seas of Night, version "
@@ -18,13 +22,16 @@ static void create(void) {
   if(!find_object(GAME_DRIVER))
     compile_object(GAME_DRIVER);
 
-  /* Load in configuration files and set data in the common and System
-     directories */
+  CONFIGD->set_game_driver(find_object(GAME_DRIVER));
+
+  /* Load configuration for game driver */
   config_unq_file();
 
-  set_up_scripting();
+  /* Set up TagD */
+  load_tagd();
 
-  CONFIGD->set_game_driver(find_object(GAME_DRIVER));
+  /* Tests a simple script */
+  set_up_scripting();
 
   /* Register a help directory for the HelpD to use */
   HELPD->new_help_directory("/usr/game/help");
@@ -37,6 +44,23 @@ static void create(void) {
   HEART_BEAT->set_up_heart_beat();
 
   LOGD->write_syslog("Configured Phantasmal from /usr/game!");
+}
+
+static mixed* load_file_with_dtd(string file_path, string dtd_path) {
+  string file_tmp, dtd_tmp;
+  object dtd;
+  mixed* dtd_unq;
+
+  dtd_tmp = read_file(dtd_path);
+  file_tmp = read_file(file_path);
+
+  dtd = clone_object(UNQ_DTD);
+  dtd->load(dtd_tmp);
+
+  dtd_unq = UNQ_PARSER->unq_parse_with_dtd(file_tmp, dtd, file_path);
+  destruct_object(dtd);
+
+  return dtd_unq;
 }
 
 static void set_up_scripting(void) {
@@ -97,4 +121,55 @@ static void load_sould(void) {
 
   file_tmp = read_file("/usr/game/sould.unq");
   SOULD->from_unq_text(file_tmp);
+}
+
+static void load_tagd(void) {
+  mixed *dtd_unq;
+  int    ctr, ctr2;
+
+  dtd_unq = load_file_with_dtd("/usr/game/tagd.unq", "/usr/game/tagd.dtd");
+
+  for(ctr = 0; ctr < sizeof(dtd_unq); ctr += 2) {
+    string tag_name, tag_get, tag_set, add_func;
+    int    tag_type;
+
+    if(typeof(dtd_unq[ctr + 1]) != T_ARRAY)
+      error("Internal error parsing TAGD file!");
+
+    switch(dtd_unq[ctr]) {
+    case "mobile_tag":
+      add_func = "new_mobile_tag";
+      break;
+    case "object_tag":
+      add_func = "new_object_tag";
+      break;
+    default:
+      error("Unknown tag '" + STRINGD->mixed_sprint(dtd_unq[ctr]) + "'!");
+    }
+
+    tag_get = tag_set = nil;
+    tag_type = -1;
+
+    for(ctr2 = 0; ctr2 < sizeof(dtd_unq[ctr + 1]); ctr2++) {
+      switch(dtd_unq[ctr + 1][ctr2][0]) {
+      case "name":
+	tag_name = STRINGD->trim_whitespace(dtd_unq[ctr + 1][ctr2][1]);
+	break;
+      case "type":
+	tag_type = dtd_unq[ctr + 1][ctr2][1];
+	break;
+      case "getter":
+	tag_get = STRINGD->trim_whitespace(dtd_unq[ctr + 1][ctr2][1]);
+	break;
+      case "setter":
+	tag_set = STRINGD->trim_whitespace(dtd_unq[ctr + 1][ctr2][1]);
+	break;
+      default:
+	error("Unrecognized label in switch for TagD UNQ: "
+	      + STRINGD->mixed_sprint(dtd_unq[ctr + 1][ctr2]) + "!");
+      }
+    }
+
+    call_other(TAGD, add_func, tag_name, tag_type, tag_get, tag_set);
+  }
 }
