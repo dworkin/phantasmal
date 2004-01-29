b996d70e939a195b42756d034cf42a61567f7f3a
angelbob <angelbob@71c14604-2bbe-4e77-8db8-9244a9b71533>
2004-01-29 01:08:09 +0000

    Latest revision of the BookBinder script.

--- website/DGD/Book/bookbinder.pl
+++ website/DGD/Book/bookbinder.pl
@@ -26,7 +26,7 @@ use strict;
 # section files by filename.  Section files are parsed individually.
 # section.list is just a list of filenames separated by newlines.  If
 # section.list is omitted, all files in the directory whose name ends
-# in ".sec" will be parsed as section files.
+# in ".sec.html" will be parsed as section files.
 
 # The book.toc file overrides ordering of chapters.  If it doesn't
 # exist, chapters and subsections use the default (ASCII) order.
@@ -47,7 +47,7 @@ use strict;
 # and so on would all need to be valid subsections, and occur
 # somewhere within the section files.
 
-# There is also one special entry in the Book.toc file.  Its first
+# There is also one special entry in the 'book.toc' file.  Its first
 # line is simply a colon followed by a newline.  On succeeding lines,
 # the chapter names are given in the planned order.  If any entry,
 # including the special entry, is omitted, the unspecified chapters or
@@ -69,12 +69,12 @@ my $sectionlist;
 if (-e "section.list") {
     $sectionlist = `cat section.list`;
 } else {
-    $sectionlist = `ls *.sec`;
+    $sectionlist = `ls *.sec.html`;
 }
 
 my $toc_contents;
 
-if(-e "book.toc") {
+if(-f "book.toc") {
     open(FILE, "<book.toc") or die "Can't open book.toc for reading: $!";
     $toc_contents = join("", <FILE>);
     close(FILE);
@@ -134,12 +134,15 @@ sub parse_toc_contents {
 
       next LINE if ($line =~ /^\s*$/);  # Skip whitespace lines
 
-      if($line =~ /^\s*(\S+)\s*:\s*$/) {
+      if($line =~ /^\s*(\S*)\s*:\s*$/) {
 	  # Parsed a section name
-	  $newsecname = $1;
+	  $newsecname = ($1 ? $1 : "");
 	  if(defined($secname)) {
 	      # Store old section info
+	      print "Setting order of '$secname' to " . join(", ", @secorder)
+		  . "\n";
 	      $chapter_order{$secname} = [ @secorder ];
+
 	      @secorder = ();
 	  }
 	  $secname = $newsecname;
@@ -165,19 +168,20 @@ sub parse_toc_contents {
 
     if(defined($secname)) {
 	$chapter_order{$secname} = [ @secorder ];
+	print "Setting order of '$secname' to " . join(", ", @secorder)
+	    . "\n";
     }
 }
 
 
-# parse_section_file parses a .sec file and adds the chapter to the
-# %chapter_jumble.  Eventually they'll be sorted and output, but this
-# function doesn't do that.
+# parse_section_file parses a .sec.html file and adds the chapter to
+# the %chapter_jumble.  Eventually they'll be sorted and output, but
+# this function doesn't do that.
 
 sub parse_section_file {
     my $sectionfile = shift;
     my ($contents, @sections, $section, $secname);
 
-    print "Found section file $sectionfile!\n";
     open(FILE, $sectionfile) or die "Can't open file $sectionfile: $!";
     $contents = join("", <FILE>);
     close(FILE);
@@ -190,16 +194,18 @@ sub parse_section_file {
       next SECT if $section =~ /^\s*$/;
 
       if($section =~ /^SECTION\s+([A-Za-z0-9.\-?!]+)\s+(.*)$/m) {
+          my $title;
+
 	  $secname = $1;
-	  print "Found section $secname\n";
-	  add_jumbled_chapter_name($secname, $2);
+	  $title = $2;
+	  add_jumbled_chapter_name($secname, $title);
       } else {
 	  die "Unknown section: '$section' in file $sectionfile!";
       }
 
       # Now cut the first line off
       die "Can't parse SECTION '$section'!"
-	  unless $section =~ /^(.*)$(.*)/m;
+	  unless $section =~ /^(.*?)$(.*)/ms;
 
     $section = $2;
 
@@ -219,26 +225,26 @@ sub parse_section_file {
 # the readable title of the section.
 
 sub add_jumbled_chapter_name {
-    my (@subsec_list, $fullname, $sec, $ent, $title);
+    my (@subsec_list, $fullname, $sec, $ent, $title, $parent);
 
     $fullname = shift;
     $title = shift;
     @subsec_list = split /\./, $fullname;
     $sec = "";
+    $parent = "";
     foreach $ent (@subsec_list) {
-	my $parent = $sec;
 	$sec .= $ent;
 	unless(defined($chapter_jumble{$sec})) {
 	    # Put in a placeholder name
-	    print "Adding section entry '$sec' to '$parent'.\n";
 	    $chapter_jumble{$sec} = [ undef, undef ];
 	    $chapter_jumble{$parent} = [@{$chapter_jumble{$parent}}, $sec];
 	}
+	$parent = $sec;
 	$sec .= ".";
     }
 
     if(defined($chapter_jumble{$fullname}->[0])) {
-	die "Repeated section $fullname in .sec files!";
+	die "Repeated section $fullname in .sec.html files!";
     }
     $chapter_jumble{$fullname}->[0] = $title;
 }
@@ -261,16 +267,47 @@ sub sort_chapters {
 	my ($title, $cont, @subsections) = @{$chapter_jumble{$section}};
 
 	if(defined($chapter_order{$section})) {
-	    # Some of these may not be defined...
-	    @subsections = @{$chapter_order{$section}};
+	    # Give warnings for any elements that aren't defined properly
+	    my (%count, %intersection, $elt);
+
+	    foreach $elt (@subsections) {
+		$count{$elt}++;
+	    }
+	    foreach $elt (@{$chapter_order{$section}}) {
+		$count{$section eq "" ? $elt : "$section.$elt"}++;
+	    }
+	    foreach $elt (keys %count) {
+		if($count{$elt} > 1) {
+		    $intersection{$elt} = 1;
+		}
+	    }
+
+	    foreach $elt (@subsections) {
+		unless($intersection{$elt}) {
+		    print "*** WARNING: Subsection '$elt' is in the section"
+			. " files, but not book.toc!\n";
+		}
+	    }
+
+	    foreach $elt (@{$chapter_order{$section}}) {
+		my $eltname = $section eq "" ? $elt : "$section.$elt";
+
+		unless($intersection{$eltname}) {
+		    print "*** WARNING: Subsection $eltname is in "
+			. "book.toc, but not the section files!\n";
+		}
+	    }
+
+	    @subsections = keys %intersection;
 	} else {
 	    @subsections = sort @subsections;
 	}
 
 	my $filename = $section;
-	$filename =~ s/\./_/;
 	if($section eq "") {
 	    $filename = "index";
+	} else {
+	    $filename =~ s/\./_/;
 	}
 
 	$chapter_jumble{$section} = [$title, $cont, $filename,
@@ -282,45 +319,136 @@ sub sort_chapters {
 
 ### Functions for .base.html output
 
+# Section Indices are things like 2.3.4 or 3.7.1.3 to indicate where
+# in the outline a given section occurs.  The %sec_index mapping maps
+# section names to their indices.
+
+my %sec_index;
+
 # write_out_html iterates through the (now-sorted) %chapter_jumble
 # mapping.  It must output sections appropriately, remembering that
 # the order in the book.toc file may include sections that don't yet
 # exist, and may fail to list sections that *do* exist.  Both cases
 # should work, but generate warnings.
 
+# Eventually, the current index.html will be replaced with a full
+# table of contents.  It's possible that other pages like a copyright
+# page and index will be added, but that's much later.
+
+# It's easier to build up a Table of Contents as we go.  This
+# variable does so, tracking through various subroutines.
+my $toc_accum;
+my $toc_indent;
+
 sub write_out_html {
-    my (@chapter_list, $chapter);
+    my (@chapter_list, $chapter, $index);
+
+    $toc_accum = "";
+    $toc_indent = 0;
+
+    # Write out various HTML files, collecting TOC entries as we go
+    ($_, $_, $_, @chapter_list) = @{$chapter_jumble{""}};
+    write_out_sections("", @chapter_list);
 
     open(FILE, ">$outdir/index.base.html")
 	or die "Can't open index file ($outdir/index.base.html) "
 	    . "for writing\n: $!";
 
-    print FILE "<h1> Title Page </h1>\n";
+    print FILE "<h1> Top-Level </h1>\n";
     print FILE "<ul>\n";
 
-    ($_, $_, $_, @chapter_list) = @{$chapter_jumble{""}};
-    foreach $chapter (@chapter_list) {
-	print FILE "  <li> <a href=\"" . $chapter_jumble{$chapter}->[2]
-		. ".html\"> "
-		    . " </a> </li>\n";
-    }
+    print FILE $toc_accum;
 
     print FILE "</ul>\n";
     close(FILE);
-
-    write_out_sections("", @chapter_list);
-
 }
 
 
 # write_out_sections takes a list of sections, which must all have the
 # same parent section.  Each will be written into its own section file,
-# with appropriate links back and forth, and appropriate lists of child
+# with appropriate links back and forth and appropriate lists of child
 # sections.
 
 sub write_out_sections {
     my $parent_section = shift;
     my @sections = @_;
 
-    
+    my ($prefix, $secnum, $index);
+    $prefix = ($parent_section eq "" ? "" : $sec_index{$parent_section});
+    unless(defined($prefix)) {
+	die "Can't get prefix for parent $parent_section!";
+    }
+
+    my $section;
+    $index = 0;
+    foreach $section (@sections) {
+	$index++;
+	$secnum = ($prefix eq "") ? ("" . $index) : "$prefix.$index";
+	$sec_index{$section} = $secnum;
+
+	$toc_accum .= (" " x $toc_indent)
+	    . "<ul> <!-- subsections of $parent_section -->\n";
+	$toc_indent += 2;
+
+	html_for_section($section, $secnum);
+
+	# If there are subsections, write their HTML files also
+	my @subsections;
+	($_, $_, $_, @subsections) = @{$chapter_jumble{$section}};
+	if(scalar(@subsections)) {
+	    write_out_sections($section, @subsections);
+	}
+
+	$toc_indent -= 2;
+	$toc_accum .= (" " x $toc_indent)
+	    . "</ul> <!-- done with $parent_section -->\n";
+    }
+}
+
+sub html_for_section {
+    my ($secname, $secnum) = @_;
+
+    my ($title, $content, $filename, @subsections)
+	= @{$chapter_jumble{$secname}};
+
+    unless(defined($title)) {
+	die "No jumble entry for '$secname'?";
+    }
+    unless(defined($filename) and $filename ne "") {
+	die "Can't get filename for section $secname!";
+    }
+    open(SECFILE, ">$outdir/$filename.base.html")
+	or die "Can't open section file $filename: $!";
+
+    print "Printing to SECFILE $filename.base.html...\n";
+
+    print SECFILE "<h2> $secnum $title </h2>\n";
+    print SECFILE $content;
+
+    $toc_accum .= (" " x $toc_indent) . "<li> <a href=\"$filename.html\"> "
+	. "$secnum $title </a> </li>\n";
+
+    # List of subsections
+    if(scalar(@subsections)) {
+	print SECFILE "<ul>\n";
+	my $subsection;
+	my $subindex = 0;
+	foreach $subsection (@subsections) {
+	    $subindex++;
+
+	    my ($sectitle, $subcont, $subfilename)
+		= @{$chapter_jumble{$subsection}};
+
+	    print SECFILE "  <li> <a href=\"$subfilename.html\"> "
+		. "$secnum.$subindex $sectitle </a> </li>\n";
+
+	}
+	print SECFILE "</ul>\n";
+    }
+
+    # Link to previous section
+
+    # Link to next section
+
+    close(SECFILE);
 }
