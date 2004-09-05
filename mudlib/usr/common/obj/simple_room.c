6c2ca0dabf15f79955d2d9d3069eec423b95d68c
angelbob <angelbob@71c14604-2bbe-4e77-8db8-9244a9b71533>
2004-09-05 17:58:00 +0000

    Made simple_room enforce the "no data field" rule.  Since more complex objects might forget the obj_type field, we should let builders know when that happens.

--- mudlib/usr/common/obj/simple_room.c
+++ mudlib/usr/common/obj/simple_room.c
@@ -21,26 +21,36 @@ static void create(varargs int clone) {
 }
 
 void destructed(int clone) {
-  room::destructed(clone);
-  unq::destructed(clone);
-  if(clone) {
-    MAPD->remove_room_object(this_object());
+  if(SYSTEM()) {
+    room::destructed(clone);
+    unq::destructed(clone);
+    if(clone) {
+      MAPD->remove_room_object(this_object());
+    }
   }
 }
 
 void upgraded(varargs int clone) {
-  room::upgraded(clone);
-  unq::upgraded(clone);
+  if(SYSTEM()) {
+    room::upgraded(clone);
+    unq::upgraded(clone);
+  }
 }
 
 string to_unq_text(void) {
-  return "~object{\n" + to_unq_flags() + "\n}\n\n";
+  if(SYSTEM() || COMMON() || GAME()) {
+    return "~object{\n  obj_type{simple room}\n" + to_unq_flags() + "\n}\n\n";
+  }
+  return nil;
 }
 
 void from_dtd_unq(mixed* unq) {
   int    ctr;
   string dtd_tag_name;
   mixed  dtd_tag_val;
+
+  if(!SYSTEM() && !COMMON() && !GAME())
+    error("Can't call from_dtd_unq unprivileged!");
 
   if(unq[0] != "object")
     error("Doesn't look like object data!");
@@ -50,8 +60,9 @@ void from_dtd_unq(mixed* unq) {
     dtd_tag_val  = unq[1][ctr][1];
 
     if(dtd_tag_name == "data") {
-      /* This is meaningful for other object subtypes, perhaps, but
-	 not for us.  Skip it. */
+      error("Simple_room should not have a data field!"
+	    + "  Did you forget an obj_type{} field in some "
+	    + "other type of room?");
     } else {
       from_dtd_tag(dtd_tag_name, dtd_tag_val);
     }
